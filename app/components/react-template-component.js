4e0aa06b5fea614c676c00ac8e63365341adaeb9
Amy Boyer <amy@zooniverse.org>
2015-12-28 14:04:52 -0600

    remove extraneous files

--- app/components/react-template-component.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import myTemplate from 'react-template-component.rt'
-
-module.exports = React.createClass({
-	getInitialState: function(){
-		return { tool: 'React Templates' };
-	},
-	render: myTemplate
-});
